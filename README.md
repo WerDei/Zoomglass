68b47b87fd4d84febf736d08b09ba17b9471ff6e
WerDei <werdei.gamedev@gmail.com>
2021-12-11 09:03:12 +0200

    Proper slot switching when spyglass is on the hotbar

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 A Fabric mod that quickly puts a spyglass into your hand with a press of a button.  
 Immersion of an in-game feature combined with a convenience of a traditional zoom mod!
 
+The default keybind is `c`, and you can find it under the "Invertory" section in keybinding settings.
+
 Requires Fabric API
--- src/main/java/net/werdei/zoomglass/client/SlotSwapper.java
+++ src/main/java/net/werdei/zoomglass/client/SlotSwapper.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
 import net.werdei.zoomglass.extensions.KeyBindingExtensions;
 
 public class SlotSwapper
@@ -14,13 +15,9 @@ public class SlotSwapper
     private final Item item;
     private final Text noItemMessage;
 
-    private boolean shouldBeSwapped;
-    private boolean isSwapped;
-    private int swappedItemSlotId;
-    private int selectedSlotId;
+    private boolean wasSwapRequested;
+    private SwapController swapController = null;
 
-    private PlayerInventory inventory;
-    private ClientPlayerEntity player;
 
     public SlotSwapper(Item item, Text noItemMessage)
     {
@@ -28,55 +25,50 @@ public class SlotSwapper
         this.noItemMessage = noItemMessage;
     }
 
-    public void tick(boolean swapState, MinecraftClient client)
+    public void tick(boolean isSwapRequested, MinecraftClient client)
     {
         if (client.player == null | client.interactionManager == null) return;
 
-        if (!shouldBeSwapped && swapState)
-            swapSlotStart(client);
-        else if (shouldBeSwapped && !swapState)
-            swapSlotFinish(client);
-        else if (isSwapped)
-            tickWhileSwapped(client);
-        shouldBeSwapped = swapState;
+        if (!wasSwapRequested && isSwapRequested)
+            swapStart(client);
+        else if (wasSwapRequested && !isSwapRequested)
+            swapFinish();
+        else if (isSwapped() && swapController.shouldSwapBack())
+            swapFinish();
+        wasSwapRequested = isSwapRequested;
     }
 
-    private void tickWhileSwapped(MinecraftClient client)
+    private void swapStart(MinecraftClient client)
     {
-        if (selectedSlotId != inventory.selectedSlot ||
-                inventory.getStack(selectedSlotId).getItem() != item)
-            swapSlotFinish(client);
-    }
+        if (isSwapped()) return;
 
-    private void swapSlotStart(MinecraftClient client)
-    {
-        if (isSwapped) return;
-
-        player = client.player;
-        inventory = player.getInventory();
-
-        swappedItemSlotId = inventory.getSlotWithStack(new ItemStack(item));
-        if (swappedItemSlotId == -1)
+        var itemSlotId = client.player.getInventory().getSlotWithStack(new ItemStack(item));
+        if (itemSlotId == -1)
         {
-            player.sendMessage(noItemMessage, true);
+            client.player.sendMessage(noItemMessage, true);
             return;
         }
-        selectedSlotId = inventory.selectedSlot;
-        swapSlots(swappedItemSlotId, selectedSlotId, client);
-        setForceUse(true, client);
-        //client.interactionManager.interactItem(player, player.world, Hand.MAIN_HAND);
-        isSwapped = true;
+
+        if (PlayerInventory.isValidHotbarIndex(itemSlotId))
+            swapController = new SwapFromHotbar(client, itemSlotId);
+        else
+            swapController = new SwapFromInventory(client, itemSlotId);
+
     }
 
-    private void swapSlotFinish(MinecraftClient client)
+    private void swapFinish()
     {
-        if (!isSwapped) return;
+        if (!isSwapped()) return;
 
-
-        setForceUse(false, client);
-        swapSlots(swappedItemSlotId, selectedSlotId, client);
-        isSwapped = false;
+        swapController.swapBack();
+        swapController = null;
     }
+
+    public boolean isSwapped()
+    {
+        return swapController != null;
+    }
+
 
 
     private static void swapSlots(int inventorySlot, int hotbarSlot, MinecraftClient client)
@@ -93,5 +85,96 @@ public class SlotSwapper
     private static void setForceUse(boolean value, MinecraftClient client)
     {
         ((KeyBindingExtensions) client.options.keyUse).setOverridePressed(value);
+    }
+
+
+
+    // Swap logic
+
+    private abstract class SwapController
+    {
+        protected PlayerInventory inventory;
+        protected ClientPlayerEntity player;
+        protected MinecraftClient client;
+        protected int itemSlotId;
+        protected int selectedSlotId;
+
+        public SwapController(MinecraftClient client, int itemSlotId)
+        {
+            this.client = client;
+            player = client.player;
+            inventory = player.getInventory();
+            this.itemSlotId = itemSlotId;
+            this.selectedSlotId = inventory.selectedSlot;
+        }
+
+        public boolean shouldSwapBack()
+        {
+            return didSelectedSlotChange() || didHandItemChange();
+        }
+
+        public abstract void swapBack();
+
+        protected final void startForceUseItem()
+        {
+            client.interactionManager.interactItem(player, player.world, Hand.MAIN_HAND);
+            setForceUse(true, client);
+        }
+
+        protected final void stopForceUseItem()
+        {
+            setForceUse(false, client);
+        }
+
+        protected final boolean didSelectedSlotChange()
+        {
+            return inventory.selectedSlot != selectedSlotId;
+        }
+
+        protected final boolean didHandItemChange()
+        {
+            return inventory.getMainHandStack().getItem() != item;
+        }
+    }
+
+
+    private class SwapFromInventory extends SwapController
+    {
+
+        public SwapFromInventory(MinecraftClient client, int itemSlotId)
+        {
+            super(client, itemSlotId);
+            swapSlots(itemSlotId, selectedSlotId, client);
+            startForceUseItem();
+        }
+
+        @Override
+        public void swapBack()
+        {
+            stopForceUseItem();
+            swapSlots(itemSlotId, selectedSlotId, client);
+        }
+    }
+
+
+    private class SwapFromHotbar extends SwapController
+    {
+        private final int previousSelectedSlotId;
+
+        public SwapFromHotbar(MinecraftClient client, int itemSlotId)
+        {
+            super(client, itemSlotId);
+            previousSelectedSlotId = selectedSlotId;
+            selectedSlotId = inventory.selectedSlot = itemSlotId;
+            startForceUseItem();
+        }
+
+        @Override
+        public void swapBack()
+        {
+            stopForceUseItem();
+            if (!didSelectedSlotChange())
+                inventory.selectedSlot = previousSelectedSlotId;
+        }
     }
 }
